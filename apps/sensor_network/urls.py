685b236ddfefaed00f2794d584b2cf2f2413b76d
Gabriel Freites <gabohd@gmail.com>
2018-06-02 23:55:08 -0400

    added sensor network models and some basic urls + fixed psycopg requirement alert

--- apps/sensor_network/urls.py
+++ apps/sensor_network/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import *
+from django.urls import re_path
+
+from .views import SNDetails, SensorPipeline, SNList
 
 app_name = 'rdf_manager'
 urlpatterns = [
+	re_path(r'^$', SNList.as_view(), name='sn_list'),
+	re_path(r'^<sn_id>/$', SNDetails.as_view(), name='sn_details'),
+	re_path(r'^<sn_id>/<sensor_uri>/$', SensorPipeline.as_view(), name='sensor_pipeline'),
 ]
--- apps/sensor_network/views.py
+++ apps/sensor_network/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views import View
+from django.views.generic import CreateView, FormView, ListView, UpdateView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from .models import SensorNetwork
 
-# Create your views here.
+
+class SNList(LoginRequiredMixin, ListView):
+    permission_required = 'is_staff'
+    login_url = '/admin/login/'
+    model = SensorNetwork
+    template_name = 'sn_list.html'
+    context_object_name = 'sns'
+
+
+class SNDetails(LoginRequiredMixin, ListView):
+    permission_required = 'is_staff'
+    login_url = '/admin/login/'
+    model = SensorNetwork
+    template_name = 'sn_list.html'
+
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SensorPipeline(View):
+	def post(self, request, sn_id, sensor_uri):
+		return self.get_context_data()
